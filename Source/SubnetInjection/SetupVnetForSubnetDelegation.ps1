f65227e5fce80a09abe850d205c0393707f88256
Oscar Faixat <osfaixat@microsoft.com>
2025-11-18 23:06:23 -0800

    make the old script slightly more usable at least...

--- Source/SubnetInjection/SetupVnetForSubnetDelegation.ps1
+++ Source/SubnetInjection/SetupVnetForSubnetDelegation.ps1
@@ -9,61 +9,54 @@ NO TECHNICAL SUPPORT IS PROVIDED. YOU MAY NOT DISTRIBUTE THIS CODE UNLESS YOU HA
 # Load thescript
 . "$PSScriptRoot\..\Common\EnterprisePolicyOperations.ps1"
 
-function SetupVnetForSubnetDelegation
-{
-    param(
-        [Parameter(
-            Mandatory,
-            HelpMessage="The Policy subscription"
-        )]
-        [string]$virtualNetworkSubscriptionId,
+param(
+    [Parameter(
+        Mandatory,
+        HelpMessage="The Policy subscription"
+    )]
+    [string]$virtualNetworkSubscriptionId,
 
-        [Parameter(Mandatory)]
-        [ValidateNotNullOrEmpty()]
-        [string]$resourceGroupName,
+    [Parameter(Mandatory)]
+    [ValidateNotNullOrEmpty()]
+    [string]$resourceGroupName,
 
-        [Parameter(Mandatory)]
-        [ValidateNotNullOrEmpty()]
-        [String]$virtualNetworkName,
+    [Parameter(Mandatory)]
+    [ValidateNotNullOrEmpty()]
+    [String]$virtualNetworkName,
 
-        [Parameter(Mandatory)]
-        [ValidateNotNullOrEmpty()]
-        [String]$subnetName
-    )
-
-    Write-Host "Logging In..." -ForegroundColor Green
-    AzureLogin
-    Write-Host "Logged In" -ForegroundColor Green
-
-    $setSubscription = Set-AzContext -Subscription $virtualNetworkSubscriptionId
-
-    Write-Host "Getting virtual network $virtualNetworkName" -ForegroundColor Green
-    $virtualNetwork = Get-AzVirtualNetwork -Name $virtualNetworkName -ResourceGroupName $resourceGroupName
-    if ($null -eq  $virtualNetwork.Name)
-    {
-         Write-Host "Virtual network not retrieved" -ForegroundColor Red
-         return
-    }
-    Write-Host "Virtual network retrieved" -ForegroundColor Green
-
-    Write-Host "Getting virtual network subnet $subnetName" -ForegroundColor Green
-    $subnet = Get-AzVirtualNetworkSubnetConfig -Name $subnetName -VirtualNetwork $virtualNetwork
-    if ($null -eq  $subnet.Name)
-    {
-         Write-Host "Virtual network subnet not retrieved" -ForegroundColor Red
-         return
-    }
-    Write-Host "Virtual network subnet retrieved" -ForegroundColor Green
-
-    Write-Host "Adding delegation for Microsoft.PowerPlatform/enterprisePolicies to subnet $subnet.Name in vnet $virtualNetworkName" -ForegroundColor Green
-    $subnet = Add-AzDelegation -Name "Microsoft.PowerPlatform/enterprisePolicies" -ServiceName "Microsoft.PowerPlatform/enterprisePolicies" -Subnet $subnet
-    Set-AzVirtualNetwork -VirtualNetwork $virtualNetwork
-
-    Write-Host "Added delegation for Microsoft.PowerPlatform/enterprisePolicies to subnet $subnet in vnet $virtualNetworkName" -ForegroundColor Green
-
-
-}
+    [Parameter(Mandatory)]
+    [ValidateNotNullOrEmpty()]
+    [String]$subnetName
+)
 
 Write-Warning "This script is deprecated. Please use New-VnetForSubnetDelegation.ps1 from the Microsoft.PowerPlatform.EnterprisePolicies module instead."
 
-SetupVnetForSubnetDelegation
+Write-Host "Logging In..." -ForegroundColor Green
+AzureLogin
+Write-Host "Logged In" -ForegroundColor Green
+
+$null = Set-AzContext -Subscription $virtualNetworkSubscriptionId
+
+Write-Host "Getting virtual network $virtualNetworkName" -ForegroundColor Green
+$virtualNetwork = Get-AzVirtualNetwork -Name $virtualNetworkName -ResourceGroupName $resourceGroupName
+if ($null -eq  $virtualNetwork.Name)
+{
+        Write-Host "Virtual network not retrieved" -ForegroundColor Red
+        return
+}
+Write-Host "Virtual network retrieved" -ForegroundColor Green
+
+Write-Host "Getting virtual network subnet $subnetName" -ForegroundColor Green
+$subnet = Get-AzVirtualNetworkSubnetConfig -Name $subnetName -VirtualNetwork $virtualNetwork
+if ($null -eq  $subnet.Name)
+{
+        Write-Host "Virtual network subnet not retrieved" -ForegroundColor Red
+        return
+}
+Write-Host "Virtual network subnet retrieved" -ForegroundColor Green
+
+Write-Host "Adding delegation for Microsoft.PowerPlatform/enterprisePolicies to subnet $subnet.Name in vnet $virtualNetworkName" -ForegroundColor Green
+$subnet = Add-AzDelegation -Name "Microsoft.PowerPlatform/enterprisePolicies" -ServiceName "Microsoft.PowerPlatform/enterprisePolicies" -Subnet $subnet
+Set-AzVirtualNetwork -VirtualNetwork $virtualNetwork
+
+Write-Host "Added delegation for Microsoft.PowerPlatform/enterprisePolicies to subnet $subnet in vnet $virtualNetworkName" -ForegroundColor Green
